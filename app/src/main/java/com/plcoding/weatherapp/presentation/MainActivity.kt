1f07f7e3fe8c1e668361a22abc119c78c8aaf615
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2023-11-22 08:39:36 +0700

    Update jetpark compose to material 3

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -6,20 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val viewModel: WeatherViewModel by viewModels()
@@ -46,31 +45,24 @@ class MainActivity : ComponentActivity() {
                        Column(
                            modifier = Modifier.fillMaxSize().background(DarkBlue)
                        ){
-                           Text("Hello teacher", modifier = Modifier.align(Alignment.End), color = Color.White )
+                           Text("Customer some of components", modifier = Modifier.align(Alignment.CenterHorizontally), color = Color.White )
                            Spacer(modifier = Modifier.height(16.dp))
-                           Column (modifier = Modifier
-                               .size(width = 200.dp, height = 50.dp)
-                               .align(Alignment.CenterHorizontally)
-                               .border(width = 2.dp, color = Color.Yellow, shape = RoundedCornerShape(50.dp)))
-                           {
-                               Text("Button", modifier = Modifier.align(Alignment.End), color = Color.White )
-                           }
-                           Button(
-                               onClick = {},
-                               modifier = Modifier
-                                   .padding(16.dp)
-                                   .background(color = Color.Green, shape = RoundedCornerShape(32.dp))
-                                   .height(50.dp)
-                                   .padding(horizontal = 20.dp)
-                               ,
-                               colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
-                               interactionSource = interactionSource,
-                               elevation = ButtonDefaults.elevation(
-                                   defaultElevation = 4.dp,
-                                   pressedElevation = 0.dp
-                               )
-                           ){
-                               Text("Login", color = Color.White)
+
+                           Surface (modifier = Modifier.fillMaxWidth().size(60.dp)){
+                               Row (modifier = Modifier.run { fillMaxSize().align(alignment = Alignment.CenterHorizontally).align(alignment = Alignment.End)}){
+                                   Column (modifier = Modifier.size(width = 120.dp, height = 45.dp)) {
+                                       FilledButton(onClick = {})
+                                   }
+                                   Column (modifier = Modifier.size(width = 120.dp, height = 45.dp)) {
+                                       OutlinedButtonExample(onClick = {})
+                                   }
+                                   Column (modifier = Modifier.size(width = 120.dp, height = 45.dp)) {
+                                       TextButtonExample(onClick = {})
+                                   }
+
+                                   BottomSheetDemo()
+                               }
+
                            }
                        }
                 }
@@ -80,3 +72,77 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun FilledButton(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
+        Text("Filled")
+    }
+}
+
+@Composable
+fun OutlinedButtonExample(onClick: () -> Unit) {
+    OutlinedButton(onClick = { onClick() }) {
+        Text("Outlined")
+    }
+}
+
+@Composable
+fun TextButtonExample(onClick: () -> Unit) {
+    TextButton(
+        onClick = { onClick() }
+    ) {
+        Text("Text Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetDemo() {
+    // [START android_compose_layout_material_bottom_sheet]
+    ModalBottomSheet(onDismissRequest = { /* Executed when the sheet is dismissed */ }) {
+        // Sheet content
+    }
+    // [END android_compose_layout_material_bottom_sheet]
+
+    // [START android_compose_layout_material_bottom_sheet2]
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show bottom sheet") },
+                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                onClick = {
+                    showBottomSheet = true
+                }
+            )
+        }
+    ) { contentPadding ->
+        // Screen content
+        // [START_EXCLUDE silent]
+        Box(modifier = Modifier.padding(contentPadding)) { /* ... */ }
+        // [END_EXCLUDE]
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                // Sheet content
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }) {
+                    Text("Hide bottom sheet")
+                }
+            }
+        }
+    }
+    // [END android_compose_layout_material_bottom_sheet2]
+}
